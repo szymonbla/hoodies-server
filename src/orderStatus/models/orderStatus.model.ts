789b18d9275ea93c82720e71a9802dc256869f95
Kuchar <sz.blazynski@gmail.com>
2022-01-02 16:31:12 +0100

    Prepare validation pipe

--- src/orderStatus/models/orderStatus.model.ts
+++ src/orderStatus/models/orderStatus.model.ts
@@ -1,11 +1,20 @@
 import { Field, ID, InputType, ObjectType } from '@nestjs/graphql'
+import { IsString } from 'class-validator'
 
 @ObjectType()
 @InputType('orderStatusInput')
 export class OrderStatus {
   @Field(() => ID)
+  @IsString()
   _id: string
 
   @Field({ description: 'Order status name' })
   orderStatusName: string
+}
+@ObjectType()
+@InputType('orderId')
+export class OrderIDStatus {
+  @IsString()
+  @Field(() => ID)
+  _id: string
 }
